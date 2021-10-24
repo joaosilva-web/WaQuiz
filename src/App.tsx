e3d33e000f79300099e9321585091be6ebf848e8
João Gustavo <joaogustavosilva2001@gmail.com>
2021-10-24 14:26:35 -0300

    add new feature: Now api returns data according to the number of questions desired by the user

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,55 @@
 import { useState, useEffect} from "react";
 import { Start } from "./components/Start";
 import { Question } from "./components/Questions";
+import { api } from "./services/api";
 import "./app.css";
+
+interface ApiReceived {
+  response_code: number;
+  results: [Question];
+}
+interface Question {
+  category: string;
+  type: string;
+  difficulty: string;
+  question: string;
+  correct_answer: string;
+  incorrect_answers: [string];
+}
 
 export function App() {
   const [step, setStep] =useState(1);
   const [activeQuestion, setActiveQuestion] = useState(0);
   const [answers, setAnswers] = useState([]);
+  const [data, setData] = useState<Question[]>([] as Question[]);
 
   const quizStartHandler = () => {
     setStep(2);
   }
 
+  useEffect(() => {
+    async function loadQuest() {
+      const { data } = await api.get<ApiReceived>(
+        `api.php?amount=${localStorage.getItem('questionsAmount')}`
+        );
+        setData(data.results);
+        console.log(data.results);
+    }
+    loadQuest();
+  },[step === 2]);
+ 
+
   return (
     <div className="App">
       {step === 1 && <Start onQuizStart={quizStartHandler}/>}
-      {step === 2 && <Question/>}
+      {step === 2 && <Question
+        data={data}
+        onsAnswersUpdate={setAnswers}
+        numberOfQuestions={data.length}
+        activeQuestion={activeQuestion}
+        onSetActiveQuestions={setActiveQuestion}
+        onSetStep={setStep}
+      />}
     </div>
   )
 }
--- src/components/Start/index.tsx
+++ src/components/Start/index.tsx
@@ -1,13 +1,25 @@
-import { Button, Card, CardContent, Typography } from "@material-ui/core";
+import { Button, Card, CardContent, TextField, Typography } from "@material-ui/core";
+import { useEffect, useState } from "react";
 
 
 
 export function Start({ onQuizStart }:any) {
+    const [amountQuestions, setAmountQuestions] = useState("");
+
+
+
     return(
         <Card sx={{minWidth: 400, display: 'flex', justifyContent: 'center', margin: "0 auto"}}>
            <CardContent sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center'}}>
-           <Typography variant="h1" sx={{fontSize: 24}}>Start Quiz</Typography>
-           <Typography variant="body1">Good luck!</Typography>
+           <Typography variant="h1" sx={{fontSize: 24}}>choose how many questions you want to answer</Typography>
+           <TextField
+          id="outlined-number"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          onChange={(event) => localStorage.setItem("questionsAmount",event.target.value)}
+          />
            <Button variant="contained" onClick={onQuizStart}>Start</Button>
            </CardContent>
         </Card>
