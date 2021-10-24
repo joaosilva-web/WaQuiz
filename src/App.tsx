fcf334bc2eccd7c693f0ba0dcf5924b4d0aacd1c
João Gustavo <joaogustavosilva2001@gmail.com>
2021-10-24 13:26:06 -0300

    Add new feature: now when click in 'start' button render Question component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,18 @@ import { Question } from "./components/Questions";
 import "./app.css";
 
 export function App() {
-  
+  const [step, setStep] =useState(1);
+  const [activeQuestion, setActiveQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
+
+  const quizStartHandler = () => {
+    setStep(2);
+  }
 
   return (
     <div className="App">
-      <Start/>
-      <Question/>
+      {step === 1 && <Start onQuizStart={quizStartHandler}/>}
+      {step === 2 && <Question/>}
     </div>
   )
 }
