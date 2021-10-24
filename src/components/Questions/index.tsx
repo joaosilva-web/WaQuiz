4b06df90dfb0965e7713bcf68d539d51bffa6560
João Gustavo <joaogustavosilva2001@gmail.com>
2021-10-24 13:21:05 -0300

    create Questions component

--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,5 +1,16 @@
-
-import { Card } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  FormLabel,
+  InputLabel,
+  Radio,
+  RadioGroup,
+  Typography,
+} from "@material-ui/core";
 import { useState, useEffect, useRef, FormEvent } from "react";
 
 export function Question() {
@@ -7,7 +18,40 @@ export function Question() {
 
   return (
     <Card>
-      Questions
+      <CardContent>
+
+        <FormControl
+          sx={{ m: 3 }}
+          component="fieldset"
+          // error={error}
+          variant="standard"
+        >
+          <FormLabel component="legend"><Typography variant="h2">Question Here</Typography></FormLabel>
+          <RadioGroup
+            aria-label="quiz"
+            name="quiz"
+            //   value={value}
+            //   onChange={handleRadioChange}
+          >
+            <FormControlLabel
+              value="best"
+              control={<Radio />}
+              label="text Answer Here!"
+            />
+          </RadioGroup>
+          <FormHelperText>helperText</FormHelperText>
+          <Button sx={{ mt: 1, mr: 1 }} type="submit" variant="contained">
+            Check Answer
+          </Button>
+        </FormControl>
+        {/* <FormControl>
+                    <InputLabel>
+                        <FormControlLabel value="best" control={<Radio />} label="The best!" onChange={() => {}} />
+                    </InputLabel>
+                    <FormHelperText>Error</FormHelperText>
+                    <Button variant="contained" onClick={nextClickHandler}>Next</Button>
+                </FormControl> */}
+      </CardContent>
     </Card>
   );
 }
